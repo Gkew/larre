f5b35188102d30a1c0cef69adbba10b17bff9e7a
AxelKallman <acke_1995@hotmail.com>
2022-05-13 13:24:45 +0200

    Routes fixed

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ProductList from "./components/ProductList";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/productlist" element={<ProductList />} />
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
