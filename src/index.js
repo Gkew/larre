f902b48e7dd7601ca073f93c25d01b9a4c5b7cec
AxelKallman <acke_1995@hotmail.com>
2022-05-16 15:15:59 +0200

    import backoffice

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ProductList from "./components/ProductList";
+import Backoffice from "./components/Backoffice/Backoffice";
+import BOProductList from "./components/Backoffice/BOProductList";
+import BOProductCreate from "./components/Backoffice/BOProductCreate";
+import BOOrders from "./components/Backoffice/BOOrders";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
@@ -11,6 +17,7 @@ root.render(
         <Route path="/" element={<App />} />
         <Route path="/productlist" element={<ProductList />} />
         <Route path="/backoffice" element={<Backoffice />} />
+        <Route path="/backoffice/orders" element={<BOOrders />} />
         <Route path="/backoffice/products" element={<BOProductList />} />
         <Route path="/backoffice/addproduct" element={<BOProductCreate />} />
       </Routes>
