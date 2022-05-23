0367a11e57d8e6c8cbb69833e0867374c53816c0
kodmeron <89447350+kodmeron@users.noreply.github.com>
2022-05-23 19:21:08 +0200

    implement

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import { Navbar, Nav } from 'react-bootstrap'
 import "../css/Navbar.css"
 import { AiOutlineShoppingCart } from 'react-icons/ai'
+import logo from "../logo/SODA_POP.png"
 const NavbarComponent = () => {
   return (
     <Navbar collapseOnSelect expand="lg" className="py-5" bg="black" variant="dark">
       <Navbar.Brand href="/" className='ps-5'>
-        <h1 className='link0'>
+        {/* <h1 className='link0'>
           <span style={{ color: '#F9CEEE' }}>S</span>
           <span style={{ color: '#CCF3EE' }}>O</span>
           <span style={{ color: '#F9F3EE' }}>D</span>
@@ -15,7 +16,7 @@ const NavbarComponent = () => {
           <span style={{ color: '#CCF3EE' }}>O</span>
           <span style={{ color: '#F9F3EE' }}>P</span>
           <span style={{ color: '#97C4B8' }}>!</span>
-        </h1>
+        </h1> */} <img src={logo} height={120} width={300} />
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav">
