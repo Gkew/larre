1ee0d142c342a77d4470767ac842278d637a708e
AxelKallman <acke_1995@hotmail.com>
2022-05-24 19:50:30 +0200

    details fixes

--- src/components/Productdetails.jsx
+++ src/components/Productdetails.jsx
@@ -3,12 +3,13 @@ import { Container, Row,  } from "react-bootstrap";
 import { useParams } from "react-router-dom";
 import Axios from "axios";
 import { Col, Card, Button } from "react-bootstrap";
-
+import { CardContent } from "@mui/material";
+import { sweFormat } from "./ProductlistUtilities/sekFormatting";
 
 export default function Productdetails () {
  
   const [details, setDetails] = useState([]);
-  const [products, setProducts] = useState([]);
+  // const [products, setProducts] = useState([]);
   const { sodasID } = useParams()
  useEffect(() => {
     console.log(sodasID)
@@ -28,19 +29,45 @@ export default function Productdetails () {
 
 
   return (
-    <div>
-      <h1>{details.name}</h1>
-      <Col sm={3} className="py-2">
-          <Card className="h-100" style={{backgroundColor: '#F9CEEE', border: 'none'}}>
-            <img variant="top" style={{ width: '150px', height: '450px', objectFit: 'scale-down', margin: 'auto' }} src={`/images/products/${details.sodasID}.png`} alt="soda" />
-            <Card.Body style={{backgroundColor: '#F9F3EE'}}>
-              <Card.Title><h1>{details.name}</h1>, <h3>{details.brand}</h3></Card.Title>
-              <Card.Text>
+    <div className="productsdetails" style={{textAlign: "center", padding: "3%"}}  >
+      <Col sm={8} style={{margin: "auto"}}    >
+       
+        <Card style={{ border: "none", backgroundColor: "#F9F3EE" }}>
+          <Card.Body style={{ backgroundColor: "#F9F3EE" }}>
+        <Card.Title>
+          {details.name}
+          
+          </Card.Title> 
+           <Card.Text>
+            {details.brand}
+           
+          </Card.Text>
 
+        
+          <img variant="top" style={{ width: '200px', height: '500px', objectFit: 'scale-down', margin: 'auto' }} src={`/images/products/${sodasID}.png`}/> 
+          <Card.Text style={{padding: "5%"}}>
+            {details.description}
+           
             </Card.Text>
-            </Card.Body>
-            </Card>
-            </Col>
+          </Card.Body>
+          <Card.Footer>
+            {sweFormat(details.price)}
+            <button
+                      style={{
+                        backgroundColor: "#FEC98F",
+                        border: "none",
+                        color: "black",
+                      }}
+                      type="button"
+                      // onClick={buy}
+                      className="mt-2 btn btn-primary float-end ms-3"
+                    >
+                      Köp
+                    </button>
+      </Card.Footer>
+          
+      </Card>
+        </Col>
     </div>
   );
 }
