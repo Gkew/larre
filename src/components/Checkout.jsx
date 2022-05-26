560def9e08ec76bc1986ae8f5ec747881af456d1
kodmeron <89447350+kodmeron@users.noreply.github.com>
2022-05-26 14:49:12 +0200

    logiken klar men input error

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -1,58 +1,130 @@
-import React from 'react'
-import { Button, Card, Col, Container, Row, Form } from 'react-bootstrap'
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Button, Card, Col, Container, Row, Form } from 'react-bootstrap';
+import axios from "axios";
+import CheckoutService from '../utils/CheckoutService';
 
 export default function Checkout() {
+  const [created, setCreated] = useState(false);
+
+  const startOrderCreate = {
+    orderID: null,
+    firstName: "",
+    lastName: "",
+    streetName: "",
+    streetNo: "",
+    zipCode: "",
+    city: "",
+    orderTime: "",
+    orderTotal: 0,
+  };
+
+  const [orders, setOrders] = useState(startOrderCreate);
+  const handleInput = (e) => {
+    const { name, value } = e.target;
+    setOrders({ ...orders, [name]: value });
+  }
+
+  const addOrder = (e) => {
+    e.preventDefault();
+
+    const data = {
+      firstName: orders.firstName,
+      lastName: orders.lastName,
+      streetName: orders.streetName,
+      streetNo: orders.streetNo,
+      zipCode: orders.zipCode,
+      city: orders.city,
+      orderTime: orders.orderTime,
+      orderTotal: orders.orderTotal,
+    };
+
+    CheckoutService.create(data)
+      .then((res) => {
+        setOrders({
+          orderID: res.data.orderID,
+          firstName: res.data.firstName,
+          lastName: res.data.lastName,
+          streetName: res.config.data.streetName,
+          streetNo: res.data.streetNo,
+          zipCode: res.data.zipCode,
+          city: res.data.city,
+          orderTime: res.data.orderTime,
+          orderTotal: res.data.orderTotal,
+        });
+        setCreated(true);
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+
+  axios.defaults.baseURL = "http://localhost:4000/api";
+
+  let navigate = useNavigate();
+
+  // const addMoreOrders = () => {
+  //   setCreated(false);
+  //   navigate("/checkout", { replace: true });
+  // };
+
   return (
-    <Form>
-      <Row className="mb-3">
-        <Form.Group as={Col} controlId="formGridEmail">
-          <Form.Label>Email</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
-        </Form.Group>
+    <Container>
+      <Card>
+        <Form>
+          <Row className="mb-3">
+            <Form.Group as={Col} controlId="formGridFirstName">
+              <Form.Label>Förnamn</Form.Label>
+              <Form.Control
+                type="text"
+                name='name'
+                placeholder="Skriv in ditt förnamn"
+                onChange={handleInput}
+                value={orders.firstName}
+                required
+              />
+            </Form.Group>
 
-        <Form.Group as={Col} controlId="formGridPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
-        </Form.Group>
-      </Row>
+            <Form.Group as={Col} controlId="formGridLastName">
+              <Form.Label>Efternamn</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Skriv in ditt efternamn"
+                onChange={handleInput}
+                value={orders.lastName}
+                required />
+            </Form.Group>
+          </Row>
 
-      <Form.Group className="mb-3" controlId="formGridAddress1">
-        <Form.Label>Address</Form.Label>
-        <Form.Control placeholder="1234 Main St" />
-      </Form.Group>
+          <Form.Group as={Col} className="mb-3" controlId="formGridAddress1">
+            <Form.Label>Adress</Form.Label>
+            <Form.Control placeholder="Läskgatan 420" />
+          </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formGridAddress2">
-        <Form.Label>Address 2</Form.Label>
-        <Form.Control placeholder="Apartment, studio, or floor" />
-      </Form.Group>
+          <Form.Group className="mb-3" controlId="formGridAddress2">
+            <Form.Label>C/o adress</Form.Label>
+            <Form.Control placeholder="Läskgatan 420" />
+          </Form.Group>
 
-      <Row className="mb-3">
-        <Form.Group as={Col} controlId="formGridCity">
-          <Form.Label>City</Form.Label>
-          <Form.Control />
-        </Form.Group>
+          <Row className="mb-3">
+            <Form.Group as={Col} controlId="formGridCity">
+              <Form.Label>Stad</Form.Label>
+              <Form.Control placeholder='Läskeborg' />
+            </Form.Group>
 
-        <Form.Group as={Col} controlId="formGridState">
-          <Form.Label>State</Form.Label>
-          <Form.Select defaultValue="Choose...">
-            <option>Choose...</option>
-            <option>...</option>
-          </Form.Select>
-        </Form.Group>
+            <Form.Group as={Col} controlId="formGridZip">
+              <Form.Label>Postnummer</Form.Label>
+              <Form.Control />
+            </Form.Group>
+          </Row>
 
-        <Form.Group as={Col} controlId="formGridZip">
-          <Form.Label>Zip</Form.Label>
-          <Form.Control />
-        </Form.Group>
-      </Row>
-
-      <Form.Group className="mb-3" id="formGridCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
-      </Form.Group>
-
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
+          <Button variant="primary" type="submit">
+            Skicka min beställning!
+          </Button>
+        </Form>
+      </Card>
+    </Container>
   )
 }
